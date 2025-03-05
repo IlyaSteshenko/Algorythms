53221222ba09e95f46e10b19e6cec917f7bbc523
IlyaSteshenko <istesenko328@gmail.com>
2025-03-05 18:58:57 +0700

    first commit

---
+++ src/Solution.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public static int romanToDecimal(String romanNum) {
+        String[] romanNumArr = romanNum.split("");
+        Map<String, Integer> romanDecimalMap = getStringIntegerMap();
+        int result = 0;
+
+        for (int i = 0; i < romanNumArr.length; i++) {
+            String currEl = romanNumArr[i];
+
+            if (i == romanNumArr.length - 1) {
+                result += romanDecimalMap.get(currEl);
+                break;
+            }
+
+            String doubleNum = currEl + romanNumArr[i + 1];
+            if (romanDecimalMap.get(doubleNum) != null) {
+                result += romanDecimalMap.get(doubleNum);
+                i += 1;
+                continue;
+            }
+            result += romanDecimalMap.get(currEl);
+        }
+
+        return result;
+    }
+
+    private static Map<String, Integer> getStringIntegerMap() {
+        Map<String, Integer> romanDecimalMap = new HashMap<>();
+        romanDecimalMap.put("I", 1);
+        romanDecimalMap.put("V", 5);
+        romanDecimalMap.put("X", 10);
+        romanDecimalMap.put("L", 50);
+        romanDecimalMap.put("C", 100);
+        romanDecimalMap.put("D", 500);
+        romanDecimalMap.put("M", 1000);
+        romanDecimalMap.put("IV", 4);
+        romanDecimalMap.put("IX", 9);
+        romanDecimalMap.put("XL", 40);
+        romanDecimalMap.put("XC", 90);
+        romanDecimalMap.put("CD", 400);
+        romanDecimalMap.put("CM", 900);
+        return romanDecimalMap;
+    }
+
+}
