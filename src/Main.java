dd4ded5f41767beea6da3c249fb149391e4113a2
IlyaSteshenko <istesenko328@gmail.com>
2025-04-08 17:13:44 +0700

    Little fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.romanToDecimal("MMXIX"));
+        int[] banknotes = {1, 2, 5, 10, 50, 100, 200, 500, 1000, 2000, 5000};
+        System.out.println(Solution.minBanknotes(banknotes, 3250));
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
@@ -48,6 +47,112 @@ public class Solution {
         romanDecimalMap.put("CD", 400);
         romanDecimalMap.put("CM", 900);
         return romanDecimalMap;
+    }
+
+    // Two Sum
+    public static int[] twoSum(int[] nums, int target) {
+        if (nums.length == 2) {
+            return new int[]{0, 1};
+        }
+
+        int[] result = new int[2];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.get(diff) != null) {
+                result[0] = i;
+                result[1] = map.get(diff);
+            }
+        }
+
+        return result;
+    }
+
+    // Add Two Numbers
+//    public static LinkedList<Integer> addTwoNumbers(
+//            LinkedList<Integer> originList, LinkedList<Integer> additionList) {
+//    }
+
+    // Математическое решение задачи
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        if (x < 10) return true;
+        if (x < 100) return x % 10 == x / 10;
+
+        int digitsCount;
+        int leftIndex = 0;
+
+        // (int) Math.log10(x) - возвращает кол-во цифр в числе минус 1
+        int rightIndex = (int) Math.log10(x);
+
+        while (leftIndex < rightIndex) {
+            if (x == 0) break;
+
+            digitsCount = (int) Math.log10(x);
+            int leftNum = x / (int) Math.pow(10, digitsCount);
+            int rightNum = x % 10;
+
+            if (leftNum != rightNum) return false;
+
+            x /= 10;
+            if (x == 0) return false;
+            x %= (int) Math.pow(10, digitsCount - 1);
+
+            leftIndex++;
+            rightIndex--;
+        }
+
+        return true;
+    }
+
+    // Бинарный поиск по массиву чисел
+    public static int binarySearchInt(List<Integer> searchableList, int requiredInt) {
+
+        if (searchableList.get(0) == requiredInt) {
+            return 0;
+        }
+        if (searchableList.get(searchableList.size() - 1) == requiredInt) {
+            return searchableList.size() - 1;
+        }
+
+        int left = 0;
+        int right = searchableList.size() - 1;
+        int mid = (right + left) / 2;
+        int iterator = 0;
+
+        while (iterator != searchableList.size()) {
+            if (searchableList.get(mid) == requiredInt) {
+                return mid;
+            } else if (searchableList.get(mid) > requiredInt) {
+                right = mid;
+                mid = (left + right) / 2;
+                iterator++;
+                continue;
+            }
+            left = mid;
+            mid = (left + right) / 2;
+            iterator++;
+        }
+
+        return -1;
+    }
+
+    // Минимальное кол-во купюр из банкомата по указанной сумме
+    public static int minBanknotes(int[] banknotes, int amount) {
+        Arrays.sort(banknotes);
+        int count = 0;
+
+        for (int i = banknotes.length - 1; i >= 0; i--) {
+            count += amount / banknotes[i];
+            amount %= banknotes[i];
+        }
+
+        return amount == 0 ? count : -1;
     }
 
 }
